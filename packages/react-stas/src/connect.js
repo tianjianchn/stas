00db7e792fcfb0d899ab046a25649bf97a62e100
kiliwalk <kilireg@qq.com>
2017-02-23 13:44:53 +0800

    Fix: wrong characteristic of connect() in react-stas

--- packages/react-stas/src/connect.js
+++ packages/react-stas/src/connect.js
@@ -3,48 +3,48 @@ const React = require('react');
 const { PureComponent, PropTypes } = React;
 const { Store } = require('stas');
 
-function connect(TargetComponent, mapToProps = defaultMapToProps) {
-  class ConnectComponent extends PureComponent {
-    static propTypes = {
-    }
+function connect(mapToProps = defaultMapToProps) {
+  return (TargetComponent) => {
+    class ConnectComponent extends PureComponent {
+      static propTypes = {
+      }
 
-    static contextTypes = {
-      store: PropTypes.instanceOf(Store).isRequired,
-    }
+      static contextTypes = {
+        store: PropTypes.instanceOf(Store).isRequired,
+      }
 
-    constructor(props, context) {
-      super(props, context);
+      constructor(props, context) {
+        super(props, context);
 
-      const { store } = context;
-      this.state = { ...this.getMapProps(store.state) };
+        const { store } = context;
+        this.state = { ...this.getMapProps(store.state) };
 
-      this._unsubscribe = store.subscribe((newState) => {
-        this.setState({ ...this.getMapProps(newState) });
-      });
-    }
+        this._unsubscribe = store.subscribe((newState) => {
+          this.setState({ ...this.getMapProps(newState) });
+        });
+      }
 
-    componentWillUnmount() {
-      if (this._unsubscribe) {
-        this._unsubscribe();
-        this._unsubscribe = null;
+      componentWillUnmount() {
+        if (this._unsubscribe) {
+          this._unsubscribe();
+          this._unsubscribe = null;
+        }
+      }
+
+      getMapProps(state) {
+        const dispatch = this.context.store.dispatch;
+        const mprops = mapToProps(state, dispatch);
+        if (!mprops) return { state, dispatch };
+        if (!mprops.dispatch) mprops.dispatch = dispatch;
+        return mprops;
+      }
+
+      render() {
+        return <TargetComponent {...this.props} {...this.state} />;
       }
     }
-
-
-    getMapProps(state) {
-      const dispatch = this.context.store.dispatch;
-      const mprops = mapToProps(state, dispatch);
-      if (!mprops) return { state, dispatch };
-      if (!mprops.dispatch) mprops.dispatch = dispatch;
-      return mprops;
-    }
-
-    render() {
-      return <TargetComponent {...this.props} {...this.state} />;
-    }
-  }
-
-  return ConnectComponent;
+    return ConnectComponent;
+  };
 }
 
 function defaultMapToProps(state, dispatch) {
